4accf54a4d104c3b8a829cbcc85017144ba4411e
W.Chao <785808933@qq.com>
2020-12-17 00:38:50 +0800

    Update Card.vue

--- src/components/global/Card.vue
+++ src/components/global/Card.vue
@@ -24,6 +24,7 @@ import { computed, defineComponent } from "vue";
 import { useRouter } from "vue-router";
 import { Movie, Performer } from "@/types/movie";
 
+// 卡片的属性定义
 interface CardProps {
   movie?: Movie;
   sort?: number;
@@ -32,23 +33,30 @@ interface CardProps {
 export default defineComponent({
   name: "Card",
   props: ["movie", "sort"],
+  // 将props设置为只读属性
   setup(props: Readonly<CardProps>, { emit }) {
+    // 获取路由对象
     const router = useRouter();
 
+    // 暴露出的属性和方法可以在模板中使用
     return {
       removeZh(str: string) {
         return str.replace("(中国大陆)", "");
       },
+      // 计算属性：将movie中的所有演员join为一个字符串赋值casts
       casts: computed(() => {
         if (!props.movie) return "未知";
         return props.movie.casts.map((it: Performer) => it.name).join(",");
       }),
+      // 排序用
       isRank: computed(() => {
         return typeof props.sort === "number";
       }),
       onClick() {
         if (!props.movie) return;
+        // 路由到具体详情页
         router.push(`/movie/${props.movie.id}`);
+        // 触发父级元素的select方法，传入参数为props.movie.id
         emit("select", props.movie.id);
       }
     };
