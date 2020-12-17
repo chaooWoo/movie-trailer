3ec49512681f8d183516ec0d04e8072dab3d9ad3
W.Chao <785808933@qq.com>
2020-12-18 00:21:16 +0800

    Update Scroll.vue

--- src/components/global/Scroll.vue
+++ src/components/global/Scroll.vue
@@ -17,30 +17,37 @@ BScroll.use(ObserveDOM).use(PullUp);
 export default defineComponent({
   name: "Scroll",
   props: {
+    // 父组件传入的数据
     data: {
       type: Array,
       default: () => []
     },
+    // 父组件传入的数据
     pullUpLoad: {
       type: [Boolean, Object],
       default: false
     }
   },
   setup(props, { emit }) {
+    // 通过ref获取dom
     const wrapper = ref<HTMLElement>();
     const scroll = ref<BScroll>();
 
     /* 初始化滚动 */
     const initScroll = () => {
+      // 父组件没有插入solt则不初始化滚动组件
       if (!wrapper.value) {
         return;
       }
+      // 构造BScroll三方组件
+      // 响应式的
       scroll.value = new BScroll(wrapper.value, {
         bounce: false,
         click: true,
         pullUpLoad: props.pullUpLoad,
         observeDOM: true
       });
+      // 触发三方组件的xxx会触发父组件的pulling-up事件
       if (props.pullUpLoad) {
         scroll.value.on("pullingUp", () => {
           emit("pulling-up");
@@ -67,6 +74,7 @@ export default defineComponent({
 
     /* keepAlive里内容变化时 */
     onActivated(() => {
+      // 被包裹组件被激活时该生命周期会被调用
       if (props.data.length) {
         refresh();
       }
